1927dd7775939ee703baa1efe905e86e7cb4ec7a
Paweł Łęczycki <pawel@msd.info.pl>
2019-12-09 22:12:05 +0100

    Update Program.cs

--- Altkom.09-11.12.19.CSharp.Basics.ConsoleProgram/Program.cs
+++ Altkom.09-11.12.19.CSharp.Basics.ConsoleProgram/Program.cs
@@ -12,25 +12,25 @@ namespace Altkom._09_11._12._19.CSharp.Basics.ConsoleProgram
         public static Program() {
             var random = new Random();
             Collection = new List<Person>() {
-                new Person() { FirstName = "Kevin", LastName = "Liu", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Martin", LastName = "Weber", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "George", LastName = "Li", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Lisa", LastName = "Miller", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Run", LastName = "Liu", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Sean", LastName = "Stewart", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Olinda", LastName = "Turner", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Jon", LastName = "Orton", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Toby", LastName = "Nixon", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Daniela", LastName = "Guinot", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Vijay", LastName = "Sundaram", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Chris", LastName = "Meyer", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Eric", LastName = "Gruber", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Piotr", LastName = "Piotrowski", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Chris", LastName = "Meyer", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Yuhong", LastName = "Li", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Yan", LastName = "Li", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Piotr", LastName = "Piotrowski", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
-                new Person() { FirstName = "Adam", LastName = "Adamski", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) }};
+                new Person() { FirstName = "Kevin", LastName = "Liu" },
+                new Person() { FirstName = "Martin", LastName = "Weber" },
+                new Person() { FirstName = "George", LastName = "Li" },
+                new Person() { FirstName = "Lisa", LastName = "Miller" },
+                new Person() { FirstName = "Run", LastName = "Liu" },
+                new Person() { FirstName = "Sean", LastName = "Stewart" },
+                new Person() { FirstName = "Olinda", LastName = "Turner" },
+                new Person() { FirstName = "Jon", LastName = "Orton" },
+                new Person() { FirstName = "Toby", LastName = "Nixon" },
+                new Person() { FirstName = "Daniela", LastName = "Guinot" },
+                new Person() { FirstName = "Vijay", LastName = "Sundaram" },
+                new Person() { FirstName = "Chris", LastName = "Meyer" },
+                new Person() { FirstName = "Eric", LastName = "Gruber" },
+                new Person() { FirstName = "Piotr", LastName = "Piotrowski" },
+                new Person() { FirstName = "Chris", LastName = "Meyer" },
+                new Person() { FirstName = "Yuhong", LastName = "Li" },
+                new Person() { FirstName = "Yan", LastName = "Li" },
+                new Person() { FirstName = "Piotr", LastName = "Piotrowski" },
+                new Person() { FirstName = "Adam", LastName = "Adamski" }};
         }
         
         private static List<Person> Collection;
