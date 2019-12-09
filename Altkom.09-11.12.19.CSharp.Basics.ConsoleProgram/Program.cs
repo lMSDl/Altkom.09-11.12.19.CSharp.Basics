3b288fa10451aa1c213db270b0834b7e44813eea
Paweł Łęczycki <pawel@msd.info.pl>
2019-12-09 22:02:43 +0100

    Update Program.cs

--- Altkom.09-11.12.19.CSharp.Basics.ConsoleProgram/Program.cs
+++ Altkom.09-11.12.19.CSharp.Basics.ConsoleProgram/Program.cs
@@ -9,11 +9,32 @@ namespace Altkom._09_11._12._19.CSharp.Basics.ConsoleProgram
 {
     class Program
     {
-        private static List<Person> Collection =
-            new List<Person>() {
-                new Person() { FirstName = "Piotr", LastName = "Piotrowski", BirthDate = new DateTime(1967, 1, 22) },
-                new Person() { FirstName = "Adam", LastName = "Adamski", BirthDate = new DateTime(2000, 11, 12) }};
-
+        public static Program() {
+            var random = new Random();
+            Collection = new List<Person>() {
+                new Person() { FirstName = "Kevin", LastName = "Liu", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Martin", LastName = "Weber", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "George", LastName = "Li", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Lisa", LastName = "Miller", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Run", LastName = "Liu", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Sean", LastName = "Stewart", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Olinda", LastName = "Turner", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Jon", LastName = "Orton", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Toby", LastName = "Nixon", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Daniela", LastName = "Guinot", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Vijay", LastName = "Sundaram", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Chris", LastName = "Meyer", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Eric", LastName = "Gruber", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Piotr", LastName = "Piotrowski", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Chris", LastName = "Meyer", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Yuhong", LastName = "Li", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Yan", LastName = "Li", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Piotr", LastName = "Piotrowski", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) },
+                new Person() { FirstName = "Adam", LastName = "Adamski", BirthDate = new DateTime(random.Next(1950, 2000), random.Next(1, 12), random.Next(1, 28)) }};
+        }
+        
+        private static List<Person> Collection;
+            
         static void Main(string[] args)
         {
             string line;
