f7c70a4d71bf26f8f6abf175c5eaaecf1883bea1
Paweł Łęczycki <pawel@msd.info.pl>
2019-12-09 12:37:40 +0100

    Initial

---
+++ Altkom.09-11.12.19.CSharp.Basics.ConsoleProgram/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._09_11._12._19.CSharp.Basics.ConsoleProgram.Models
+{
+    public class Person
+    {
+        public Person()
+        {
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"Imię: {FirstName} Nazwisko: {LastName}";
+        }
+    }
+}
---
+++ Altkom.09-11.12.19.CSharp.Basics.ConsoleProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Altkom._09_11._12._19.CSharp.Basics.ConsoleProgram.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Altkom._09_11._12._19.CSharp.Basics.ConsoleProgram
+{
+    class Program
+    {
+        private static List<Person> Collection =
+            new List<Person>() {
+                new Person() { FirstName = "Adam", LastName = "Adamski" },
+                new Person() { FirstName = "Piotr", LastName = "Piotrowski" },
+                new Person() };
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < Collection.Count; i++)
+            {
+                Console.WriteLine($"{Collection[i].LastName} {Collection[i].FirstName}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+ 
