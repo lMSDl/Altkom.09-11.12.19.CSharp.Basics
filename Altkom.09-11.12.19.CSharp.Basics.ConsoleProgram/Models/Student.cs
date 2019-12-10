9db9da75c4186a1685458a680445754c06da3bc9
Paweł Łęczycki <pawel@msd.info.pl>
2019-12-10 10:38:43 +0100

    Dodanie studenta

---
+++ Altkom.09-11.12.19.CSharp.Basics.ConsoleProgram/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._09_11._12._19.CSharp.Basics.ConsoleProgram.Models
+{
+    public class Student : Person, IStudent
+    {
+        public Student(string firstName, string lastName, float averageGrade) : base(firstName, lastName)
+        {
+            AverageGrade = averageGrade;
+        }
+
+        public float AverageGrade { get; set; }
+
+        public string StudentInfo()
+        {
+            return AverageGrade.ToString();
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format($"{base.ToString()} {{0, 15}}", AverageGrade.ToString("N1"));
+        }
+    }
+}
