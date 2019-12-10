72a9a715e5d5f78f6e124828918d434fe00de3a6
Paweł Łęczycki <pawel@msd.info.pl>
2019-12-10 11:26:02 +0100

    Rozbudowa klasy

--- Altkom.09-11.12.19.CSharp.Basics.ConsoleProgram/Models/Class.cs
+++ Altkom.09-11.12.19.CSharp.Basics.ConsoleProgram/Models/Class.cs
@@ -6,13 +6,25 @@ using System.Threading.Tasks;
 
 namespace Altkom._09_11._12._19.CSharp.Basics.ConsoleProgram.Models
 {
-    public class Class : IInstructor
+    public class Class : IInstructor, IStudent
     {
         public Instructor Instructor { get; set; }
+        public List<Student> Students { get; set; }
 
         public string InstructorInfo()
         {
             return $"{Instructor.LastName} {Instructor.FirstName}";
         }
+
+        public string StudentInfo()
+        {
+            var averageGrage = (from student in Students
+                            select student.AverageGrade).Average();
+            //averageGrage = Students.Select(x => x.AverageGrade).Average();
+
+            return averageGrage.ToString("N1");
+        }
+        
+    
     }
 }
