715d46d8acbc43e084e75af6c046239c50d69c6e
Paweł Łęczycki <pawel@msd.info.pl>
2019-12-11 15:39:11 +0100

    WPF demo

---
+++ Altkom.09-11.12.2019.CSharp.Basics.WpfApp/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Altkom._09_11._12._2019.CSharp.Basics.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Altkom.09-11.12.2019.CSharp.Basics.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Altkom._09_11._12._2019.CSharp.Basics.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var openDialog = new OpenFileDialog
+            {
+                Filter = "Json files|*.json",
+            };
+            var result = openDialog.ShowDialog();
+            if (result.HasValue && result.Value)
+            {
+                using (var file = new FileStream(openDialog.FileName, FileMode.Open))
+                {
+                    using (var streamReader = new StreamReader(file))
+                        JsonTextBlock.Text = streamReader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
