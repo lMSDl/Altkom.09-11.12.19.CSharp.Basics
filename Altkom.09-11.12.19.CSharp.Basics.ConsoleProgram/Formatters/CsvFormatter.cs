e9e82710e9d8ebfa97b0bb8b5804885d617872de
Paweł Łęczycki <pawel@msd.info.pl>
2019-12-11 11:39:09 +0100

    CSV

---
+++ Altkom.09-11.12.19.CSharp.Basics.ConsoleProgram/Formatters/CsvFormatter.cs
@@ -0,0 +1,23 @@
+﻿using ServiceStack.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._09_11._12._19.CSharp.Basics.ConsoleProgram.Formatters
+{
+    public class CsvFormatter
+    {
+        public string ToCsv<T>(IEnumerable<T> obj)
+        {
+            return CsvSerializer.SerializeToCsv(obj);
+        }
+
+        public T FromCsv<T>(string csv)
+        {
+            return CsvSerializer.DeserializeFromString<T>(csv);
+        }
+    }
+}
